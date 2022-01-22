b6a34dff98070cf923c58b5affe201130edf80c3
wilsonfreitas <wilson.freitas@gmail.com>
2022-01-22 18:07:48 -0300

    Added AnbimaVnaTPF

--- kyd/parsers/anbima.py
+++ kyd/parsers/anbima.py
@@ -5,6 +5,7 @@ from itertools import dropwhile
 from io import StringIO
 
 import lxml.html
+from lxml import etree
 
 from . import PortugueseRulesParser2
 
@@ -12,20 +13,17 @@ from . import PortugueseRulesParser2
 class AnbimaTPF:
     def __init__(self, fname):
         self.fname = fname
+        self.encoding = 'latin1'
         self.instruments = []
         self.pp = PortugueseRulesParser2()
         self.parse()
 
     def parse(self):
-        if isinstance(self.fname, str):
-            with open(self.fname, 'rb') as fp:
-                self._parse(fp)
-        else:
-            self._parse(self.fname)
+        with open(self.fname, 'r', encoding=self.encoding) as fp:
+            self._parse(fp)
 
     def _parse(self, fp):
-        _map_txt = map(lambda x: (x[0], x[1].decode('ISO-8859-1')), enumerate(fp))
-        _drop_first_n = dropwhile(lambda x: x[0] < 3, _map_txt)
+        _drop_first_n = dropwhile(lambda x: x[0] < 3, enumerate(fp))
         _drop_empy = filter(lambda x: x[1].strip() != '', _drop_first_n)
         for _, line in _drop_empy:
             row = line.split('@')
@@ -46,6 +44,56 @@ class AnbimaTPF:
     @property
     def data(self):
         return self.instruments
+
+
+class AnbimaVnaTPF:
+    def __init__(self, fname):
+        self.fname = fname
+        self.encoding = 'latin1'
+        self.pp = PortugueseRulesParser2()
+        self._data = []
+        self.parse()
+
+    def parse(self):
+        with open(self.fname, 'r', encoding=self.encoding) as fp:
+            parser = etree.HTMLParser()
+            tree = etree.parse(fp, parser)
+            self._data.append(self._parse_vna_node(tree, 'listaNTN-B'))
+            self._data.append(self._parse_vna_node(tree, 'listaNTN-C'))
+            self._data.append(self._parse_vna_node(tree, 'listaLFT'))
+
+    def _parse_vna_node(self, tree, id):
+        trs = tree.xpath(f"//div[@id='{id}']/*/table/tr")
+        if len(trs) == 0:
+            return {}
+        instrument_ref = get_all_node_text(trs[0])
+        date = get_all_node_text(trs[1][1])
+        index_ref = get_all_node_text(trs[2][1])
+        value = get_all_node_text(trs[3][1])
+        rate_value = get_all_node_text(trs[3][2])
+        try:
+            projection = get_all_node_text(trs[3][3]) == 'P'
+            rate_date = get_all_node_text(trs[3][4])
+        except IndexError:
+            projection = False
+            rate_date = ''
+        return {
+            'refdate': self.pp.parse(date),
+            'value': self.pp.parse(value),
+            'rate': self.pp.parse(rate_value),
+            'rate_start_date': self.pp.parse(rate_date),
+            'proj': projection,
+            'index_ref': index_ref,
+            'instrument_ref': instrument_ref
+        }
+
+    @property
+    def data(self):
+        return [x for x in self._data if x]
+
+
+def get_all_node_text(node):
+    return ''.join(x.strip() for x in node.itertext())
 
 
 def parse_titpub(text, buf):
