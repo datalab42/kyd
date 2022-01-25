75ff529ef635a34fab9e450ba6d1cd2bb40a50c2
wilsonfreitas <wilson.freitas@gmail.com>
2022-01-25 17:55:05 -0300

    Added StockIndexInfo

--- kyd/parsers/b3.py
+++ kyd/parsers/b3.py
@@ -1,9 +1,8 @@
 
 import json
-import logging
 from itertools import groupby
-from os import read
 
+import pandas as pd
 from lxml import etree
 
 from . import PortugueseRulesParser2, read_fwf, FWFFile, FWFRow, Field, DateField, NumericField
@@ -459,3 +458,46 @@ class BVBG087:
     @property
     def data(self):
         return self.indexes
+
+
+class StockIndexInfo:
+    def __init__(self, fname):
+        self.fname = fname
+        self._table = None
+        self.parse()
+
+    def parse(self):
+        with open(self.fname) as fp:
+            self._data = json.loads(fp.read())
+
+        df = pd.DataFrame(self._data['results'])
+
+        def _(dfx):
+            indexes = dfx['indexes'].str.split(',').explode()
+            return pd.DataFrame({
+                'company': dfx['company'],
+                'spotlight': dfx['spotlight'],
+                'code': dfx['code'],
+                'indexes': indexes,
+            })
+
+        dfr = (df.groupby(['company', 'spotlight', 'code'])
+                 .apply(_)
+                 .reset_index(drop=True))
+
+        dfr['refdate'] = self._data['header']['update']
+        dfr['duration_start_month'] = self._data['header']['startMonth']
+        dfr['duration_end_month'] = self._data['header']['endMonth']
+        dfr['duration_year'] = self._data['header']['year']
+
+        dfr = dfr.rename(columns={
+            'company': 'corporation_name',
+            'spotlight': 'specification_code',
+            'code': 'symbol'
+        })
+
+        self._table = dfr
+
+    @property
+    def data(self):
+        return self._table
